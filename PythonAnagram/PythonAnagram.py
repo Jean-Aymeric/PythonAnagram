326506de9e239a45ac9181f5854f6748e607f844
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-28 12:50:46 +0100

    done

--- PythonAnagram/PythonAnagram.py
+++ PythonAnagram/PythonAnagram.py
@@ -1,1 +1,24 @@
 
+def anagram( listLetters ) :
+    listAnagram = []
+    if len(listLetters) == 1 :
+        listAnagram.append(listLetters[0])
+    else :
+        for i in range(0, len(listLetters)) :
+            underListLetters = []
+            for j in range(len(listLetters)) :
+                 if j != i :
+                    underListLetters.append(listLetters[j])
+            underListAnagram = anagram(underListLetters)
+            for j in range (len(underListAnagram)) :
+                listAnagram.append (listLetters[i] + underListAnagram[j])
+    return listAnagram
+
+word = input("Entrez une chaine de caractère : ")
+listLetters = []
+for i in range(0, len(word)) :
+    listLetters.append(word[i])
+
+listAnagrams = anagram(listLetters)
+for i in range (len(listAnagrams)) :
+    print (listAnagrams[i])
